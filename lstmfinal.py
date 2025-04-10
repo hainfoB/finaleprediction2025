2ccf9bfe4f99e5bd35db5ea4dd578d32f2707ff1
hainfoB <haithemcomputing@gmail.com>
2025-04-10 11:58:15 +0100

    Fix: Correction de l'import de HyperParameters pour keras_tuner

--- lstmfinal.py
+++ lstmfinal.py
@@ -12,7 +12,7 @@ from keras.layers import LSTM, Dense, Dropout, Bidirectional
 from keras.callbacks import EarlyStopping
 from keras.optimizers import Adam
 from keras_tuner.tuners import RandomSearch
-from keras_tuner.tuners.hyperparameters import HyperParameters
+from keras_tuner import HyperParameters
 import keras.backend as K
 import datetime
 
